6062db6ed0f568efe899648bff95533f1b109e26
Berat Vıcıl <beratvicil@gmail.com>
2022-10-05 05:01:27 +0300

    scaffold react babel with wepback

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <h1 className='heading__one'>React application!!!</h1>
+      <button>Btn</button>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+// webpack.config.js
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+	entry: "./src/main.js",
+	output: {
+		path: path.join(__dirname, "/dist"),
+		filename: "bundle.[contenthash].js",
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./public/index.html"
+		}),
+		new MiniCssExtractPlugin({
+			filename:"style.[contenthash].css",
+		})
+	]
+};
